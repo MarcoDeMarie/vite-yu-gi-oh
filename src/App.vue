1a5a394a6485908a05fa614f99e5f18fd2aea550
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-05-04 15:14:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import axios from 'axios'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import { store } from './data/store' 
+
+export default {
+  name: 'App',
+  components:{
+    Header,
+    Main,
+  }
+
+}
+</script>
+
+<template>
+  <Header />
+
+  <Main />
+  
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<script>
+import Cards from './partials/Cards.vue'
+export default {
+  name: 'Main',
+
+  components:{
+    Cards,
+  }
+
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
